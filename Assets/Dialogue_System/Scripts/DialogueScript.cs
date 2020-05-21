bb2e4db6ae6962324e646fcc602e53e7d525babc
GGkas <gkeoof@gmail.com>
2020-05-21 17:21:53 +0300

    Finished Dialogue Script
    Finished a possible dialogue controller. Could add more into it. NEED TO MAKE JSON PARSERS

---
+++ Assets/Dialogue_System/Scripts/DialogueScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class DialogueScript : MonoBehaviour
+{
+    public CanvasGroup dialogueCanvasGroup;
+    //this could be replaced by a json filename, which we will load and then parse
+    //public string JSONFilePath;
+    public TMP_Text dialogueText;
+    public TMP_Text dialogueTitle;
+
+    public static DialogueScript instance; // singleton
+
+    public static void PopUpDialogue(TMP_Text _title, TMP_Text _text)
+    {
+        instance.dialogueText = _text;
+        instance.dialogueTitle = _title;
+        instance.dialogueCanvasGroup.DOFade(1, 0.5f);
+
+    }
+
+    public void DismissDialogue()
+    {
+        instance.dialogueCanvasGroup.DOFade(0, 0.5f).OnComplete(instance.hideCanvasGroup);
+    }
+
+    public void hideCanvasGroup()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
